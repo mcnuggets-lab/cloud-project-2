fd729a984320537ef233cf93c021083b68dc09a2
Kit-Ho Mak <mcnuggets23@gmail.com>
2024-07-07 13:02:26 +0800

    website complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cloud-project-2
+
+Model from https://huggingface.co/mrm8488/bert-tiny-5-finetuned-squadv2
--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask import render_template
+from markupsafe import escape
+from transformers import pipeline
 
 app = Flask(__name__)
 
+qa_pipeline = pipeline(
+    "question-answering",
+    model="model/",
+    tokenizer="model/"
+)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index_page():
+    """Render the form for QA model."""
+    return render_template("index.html")
+
+
+@app.route("/answer", methods=["POST"])
+def predict():
+    """Predict from input of the form."""
+    prediction = qa_pipeline({
+        "context": escape(request.form.get("context")) or "I'm Kit-Ho Mak. I come from Hong Kong.",
+        "question": escape(request.form.get("question")) or "What is my name?",
+    })
+    
+    return jsonify(prediction)
