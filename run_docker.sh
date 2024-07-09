e91f1a0e815bb04e7c54f6eed8b2a6b9e39cf0a6
Kit-Ho Mak <mcnuggets23@gmail.com>
2024-07-09 15:17:14 +0800

    small fix

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 
 # Build image
 #change tag for new container registery, gcr.io/bob
-docker build --tag=mcnuggets23/cloud-project-2 . 
+docker build --tag=mcnuggets23/cloud-project2 . 
 
 # Run flask app
-docker run -p 8080:8080 mcnuggets23/cloud-project-2
+docker run -p 8080:8080 mcnuggets23/cloud-project2
